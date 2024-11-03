5fd539ca1b7deb2e3090fd47e2c8c0486f042639
GhostVista <baconville.network@gmail.com>
2024-11-03 11:41:03 -0800

    Add files via upload

---
+++ browser-check.js
@@ -0,0 +1,5 @@
+// Browser detection for Internet Explorer 11
+var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+if (isIE11) {
+    window.location.href = 'unsupported.html'; // Redirect to the unsupported page
+}
